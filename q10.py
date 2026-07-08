52e1b0bd7dddba8f8b6f785299cabd42d2d60b13
Prajakta Botre <prachibotre111@gmail.com>
2026-07-08 16:22:29 +0530

    Delete q10.py

--- q10.py
+++
@@ -1,6 +0,0 @@
-# Write a program to calculate the area of the eqilateral triangle.
-
-side=float(input("Enter the side of the euilateral triangle:"))
-area=side**2*(3**0.5)/4
-print("The area of the equilateral triangle is: ",area)
-
