084620bfffddd9993c07b36f9d7c1d6531115127
Prajakta Botre <prachibotre111@gmail.com>
2026-07-08 16:25:46 +0530

    Delete q4.py

--- q4.py
+++
@@ -1,9 +0,0 @@
-# Write a program to enter P,T,R and calculate simple interest.
-P=float(input("Enter the principle amount: "))
-T=float(input("Enter the time period: "))
-R=float(input("Enter the rate  of interest: "))
-
-# calculating simple interest
-SI=(P*T*R)/100
-
-print("The simple interest is:", SI)
