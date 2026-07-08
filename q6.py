2c5604e42006b27f48e5104d40fc36fa176e1499
Prajakta Botre <prachibotre111@gmail.com>
2026-07-08 16:24:35 +0530

    Delete q6.py

--- q6.py
+++
@@ -1,6 +0,0 @@
-# Write a program to input two angles from user and find third angle of the triangle
-
-angle1=float(input("Enter the first angle of the triangle in degree:"))
-angle2=float(input("Enter the second angle of the triangle in degree:"))
-angle3=180-(angle1+angle2)
-print("The third angle of the triangle is:" ,angle3)
