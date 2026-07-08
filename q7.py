7508e3b6dd5489b143dc30451c0ab89ccf38f77b
Prajakta Botre <prachibotre111@gmail.com>
2026-07-08 16:26:16 +0530

    Delete q7.py

--- q7.py
+++
@@ -1,5 +0,0 @@
-# Write a program to find roots of the Quadratic equation.
-root1=float(input("Enter the first root of the quadratic equation: "))
-root2=float(input("Enter the second root of the quadratic equation: "))
-
-print("The roots of the quadratic equation are: ", root1, " and ", root2)
